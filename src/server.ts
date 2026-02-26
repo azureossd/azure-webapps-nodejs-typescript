64f39cbd13dbc140576132015aac800b6ba920ea
Keegan DSouza <kedsouza@microsoft.com>
2026-02-26 10:32:48 -0600

    test push

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World from TypeScript!'));
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log('Example app listening on port!'));
+app.listen(port, () => console.log('Example app listening on port! '));
